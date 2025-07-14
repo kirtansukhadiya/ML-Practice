e339f644b4a9b29dd3f8abd0dda2db7f640b0e2f
kirtansukhadiya <kirtanskh@gmail.com>
2025-07-14 18:59:38 +0530

    did handling missing data, catorical data, Indipendent variable, and dependent variable

--- DataPrepossing/main.py
+++ DataPrepossing/main.py
@@ -3,11 +3,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.impute import SimpleImputer # For handling missing data
+from sklearn.compose import ColumnTransformer # For encoding categorical data
+from sklearn.preprocessing import OneHotEncoder # For encoding categorical data
 
 #importing the dataset
 dataset = pd.read_csv('/Users/kirtansukhadiya/Desktop/Hub/ML-Practice/DataPrepossing/Data.csv')
 X = dataset.iloc[:, :-1].values  # Features
-Y = dataset.iloc[:, -1].values  # Target variable
+y = dataset.iloc[:, -1].values  # Target variable
 
-print(X)
-print(Y)
+#print(X)
+#print(y)
+
+# Handling missing data
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+#print(X)
+
+# Encoding categorical data
+
+# Encoding the Independent Variable
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder= 'passthrough') 
+X = np.array(ct.fit_transform(X))
+
+#print(X)
+
+# Encoding the Dependent Variable
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+print(y)
