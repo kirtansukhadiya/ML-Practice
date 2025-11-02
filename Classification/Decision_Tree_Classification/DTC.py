b653d46f34336fb11c8c7822f35ce4570100ae35
kirtansukhadiya <kirtanskh@gmail.com>
2025-11-01 23:25:41 -0400

    did Decision Tree Classification

---
+++ Classification/Decision_Tree_Classification/DTC.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score    
+from sklearn.preprocessing import StandardScaler
+
+dataset = pd.read_csv('Classification/Decision_Tree_Classification/Social_Network_Ads.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+classifier = DecisionTreeClassifier(criterion='entropy', random_state=0 )
+classifier.fit(X_train, y_train)
+print(classifier.predict(sc.transform([[30, 87000]])))
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1)) 
+cm = confusion_matrix(y_test, y_pred)
+ac = accuracy_score(y_test, y_pred)
+print(cm, ac)
