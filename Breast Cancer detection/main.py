c764262af3cfc1f735cb112b727d1fe55259c90f
kirtansukhadiya <kirtanskh@gmail.com>
2025-11-02 23:06:53 -0500

    did kmeans clustering

--- Breast Cancer detection/main.py
+++ Breast Cancer detection/main.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
 
-dataset = pd.read_csv('Breast Cancer detection using Logistic Regression/breast_cancer.csv')
+dataset = pd.read_csv('Breast Cancer detection/breast_cancer.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
