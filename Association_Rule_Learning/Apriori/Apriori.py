64f406a34c22583fceaa61a84c5618b00eea67bf
kirtansukhadiya <kirtanskh@gmail.com>
2025-11-03 23:40:42 -0500

    did Association rule - Apriori

---
+++ Association_Rule_Learning/Apriori/Apriori.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Datapreprocessing
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header=None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+
+#Training the Apriori modal on dataset
+from apyori import apriori
+rules = apriori(transactions = transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2, max_lemgth = 2)
+
+#Visualising the results
+
+#Displaying the first results coming directly fromthe output of the aprioi function
+results = list(rules)
+results
+
+#Putting the results well organised into a panda DataFrame
+def inspect(results):
+    lhs         = [tuple(result[2][0][0])[0] for result in results]
+    rhs         = [tuple(result[2][0][1])[0] for result in results]
+    supports    = [result[1] for result in results]
+    confidences = [result[2][0][2] for result in results]
+    lifts       = [result[2][0][3] for result in results]
+    return list(zip(lhs, rhs, supports, confidences, lifts))
+resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Left Hand Side', 'Right Hand Side', 'Support', 'Confidence', 'Lifts'])
+
+# Displaying results non sorted
+resultsinDataFrame
+
+# Displaying the results sorted by descending lifts
+resultsinDataFrame.nlargest(n = 10, columns = 'Lifts')
+
+
